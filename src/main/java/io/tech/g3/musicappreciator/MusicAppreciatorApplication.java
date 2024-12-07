1106be5c1c093d4ec6d15bc14255f1058cab248c
Gert Ehlers <gert.ehlers@rabobank.nl>
2024-12-07 15:05:45 +0100

    init

---
+++ src/main/java/io/tech/g3/musicappreciator/MusicAppreciatorApplication.java
@@ -0,0 +1,13 @@
+package io.tech.g3.musicappreciator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppreciatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppreciatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tech/g3/musicappreciator/LyricAppreciatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tech.g3.musicappreciator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricAppreciatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
